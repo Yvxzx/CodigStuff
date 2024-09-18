05b0368905dd2e1a5f4ff8d77e9436c86e647d34
Yvxzx <yves.jsros@gmail.com>
2024-09-18 20:58:32 +0200

    emptied proj 1

---
+++ TestProj2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TestProj2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Console.WriteLine("Player 1, please enter your name");
+        p1.setUsername(Console.ReadLine());
+        Console.WriteLine("Player 2, please enter your name");
+        p2.setUsername(Console.ReadLine());
+        Console.WriteLine($"Hello {p1.username}");
+        Console.WriteLine($"Hello {p2.username}");
+        Console.WriteLine($"{p1.username} , {p2.username}\n{p1.score} , {p2.score}");
+
+        
+    }
+    public class Player
+    {
+        public string username {get; set;}
+        public void setUsername(string username)
+        {
+            this.username = username;
+        }
+        public int score {get; set;} = 0;
+
+    }
+}
