d26c71300437948d0ca6b509c974b437f9648442
Yvxzx <yves.jsros@gmail.com>
2024-09-18 20:55:44 +0200

    created project 2

---
+++ TestProj1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleGame
+{
+    public class Program
+    {
+        Player p1 = new Player();
+
+        
+        
+    }
+
+    public class Player
+    {
+        public string? name;
+        public void setName(string inpname)
+        {
+            this.name = inpname;
+        }
+
+    }
+}
